e716f8925ab14d2099667590a8bf58d58e60b77b
mori <mori.norabadi@gmail.com>
2024-12-29 17:03:29 +0330

    create nav bar

--- src/app/components/Nav/Nav.tsx
+++ src/app/components/Nav/Nav.tsx
@@ -1,27 +1,32 @@
-export default function Nav() {
-  const links = [
-    { id: "1", href: "aboutme", text: "MORI" },
-    { id: "2", href: "projects", text: "projects" },
-    { id: "3", href: "skills", text: "skills" },
-    { id: "4", href: "workExperience", text: "experience" },
-    { id: "5", href: "contactInformation", text: "contact" },
-  ];
+"use client";
+import { slide as Menu } from "react-burger-menu";
 
-  return (
-    <div className="fixed top-0 left-0 w-screen z-20 bg-gradient-to-b from-[#000000dd] to-[#00000000]">
-      <div className="text-lg md:text-2xl p-2 md:p-6 justify-between flex w-screen max-w-2xl font-bold  px-6 m-auto">
-        {links.map((link, index) => (
-          <a
-            href={`#${link.href}`}
-            key={link.id}
-            className={`text-tGreen hover:text-tGreen ${
-              +(index === 0) ? " font-bold text-xl md:text-3xl" : " "
-            }`}
-          >
-            {link.text}
-          </a>
-        ))}
-      </div>
-    </div>
-  );
+export default function Nav() {
+	const links = [
+		{ id: "1", href: "projects", text: "projects" },
+		{ id: "2", href: "skills", text: "skills" },
+		{ id: "3", href: "workExperience", text: "experience" },
+		{ id: "4", href: "contactInformation", text: "contact" },
+	];
+
+	return (
+		<>
+			<div className="fixed top-0 h-20 left-0 w-screen z-20 bg-gradient-to-b from-[#000000dd] to-[#00000000]">
+				<div className=" text-tGreen p-6 text-3xl">
+					<a href="#about">mori</a>
+				</div>
+			</div>
+			<Menu right>
+				{links.map((link) => (
+					<a
+						href={`#${link.href}`}
+						key={link.id}
+						className="text-tGreen hover:text-tGreen p-1 text-lx underline"
+					>
+						{link.text}
+					</a>
+				))}
+			</Menu>
+		</>
+	);
 }
